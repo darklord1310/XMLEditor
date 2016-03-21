d8c228cdd9a4dcc029ef6f7f580b8de88b030f7f
JacksonDMC <jackson_dmc69@hotmail.com>
2016-03-21 18:02:23 +0800

    Save progress on extracting data from Excel

--- XMLEditor/Form1.cs
+++ XMLEditor/Form1.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace XMLEditor
 {
     public partial class Form1 : Form
     {
+        Excel.Application xlApp;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:/Users/User/Desktop/V5S_Diag Command.xls");
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[2];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int rowCount = xlRange.Rows.Count;
+            int colCount = xlRange.Columns.Count;
+            MessageBox.Show(rowCount.ToString() + " " + colCount.ToString());
+
+            for (int i = 1; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= colCount; j++)
+                {
+                    if (xlRange.Cells[i, j].Value2 != null)
+                        MessageBox.Show(xlRange.Cells[i, j].Value2.ToString());
+                }
+            }
         }
     }
 }
---
+++ XMLEditor/TestMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLEditor
+{
+    public class TestMenu
+    {
+        private string  catergoryName;
+        private char    categoryID;
+        private string  moduleName;
+        private string  moduleID;
+        private string  funcName;
+
+        public TestMenu()
+        {
+
+        }
+    }
+}
